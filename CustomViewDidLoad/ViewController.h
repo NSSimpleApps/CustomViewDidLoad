8d25073b58d57e30dee2325be674bbdeaf25563c
NSSimpleApps <ns.poluyanov.sergey@gmail.com>
2015-04-19 20:25:35 +0600

    Initial Commit

---
+++ CustomViewDidLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomViewDidLoad
+//
+//  Created by neo on 19.04.15.
+//  Copyright (c) 2015 NSSimpleApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
